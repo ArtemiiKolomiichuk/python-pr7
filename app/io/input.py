828ff72ee159e2f8d3d3f4482dd339205ef5c5f3
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2024-03-18 21:35:12 +0200

    Add input and output functions

---
+++ app/io/input.py
@@ -0,0 +1,9 @@
+
+def input_from_console():
+    pass 
+
+def input_from_file(file_path):
+    pass
+
+def input_from_file_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,9 @@
+
+def write_in_console(message):
+    pass
+
+def write_in_file(file_path, message):
+    pass
+
+def write_in_file_pandas(file_path, message):
+    pass
